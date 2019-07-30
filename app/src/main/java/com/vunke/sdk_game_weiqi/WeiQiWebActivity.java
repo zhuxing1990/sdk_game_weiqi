ba2b983861b1a7771b57105df1a8a3b11d662cc4
zhuxing1993927@163.com <zhuxing1993927@163.com>
2019-07-30 17:05:02 +0800

    update version code and update styles

--- app/src/main/java/com/vunke/sdk_game_weiqi/WeiQiWebActivity.java
+++ app/src/main/java/com/vunke/sdk_game_weiqi/WeiQiWebActivity.java
@@ -31,6 +31,9 @@ public class WeiQiWebActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_weiqiweb);
         initWebView();
         getAppIntent(getIntent());
